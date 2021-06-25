763c43c3c7d6861351e30863513a448019c69fd4
Quang Nguyen <wtfbom456@gmail.com>
2021-06-25 13:37:40 +0700

    Add project files.

---
+++ Hello_World/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Human!");
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
